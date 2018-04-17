a16affcd620ce5fd711c77b7e8c07f1f991b1308
t802 <trevor.lacombe@gmail.com>
2018-04-16 20:04:40 -0400

    2018/04/16 v1
    - added files to new repository

---
+++ JobPost.java
@@ -0,0 +1,51 @@
+package com.example.user.OpusRimor;
+
+
+public class JobPost {
+    private String title, site, PostDate, JLink, Desc;
+
+    public JobPost() {
+    }
+    // creates an object that contains all the information collected for a particular job post
+    public JobPost(String title, String site, String PostDate, String JLink, String Desc) {
+        this.title = title;
+        this.PostDate = PostDate;
+        this.site = site;
+        this.JLink = JLink;
+        this.Desc = Desc;
+    }
+
+    // series of methods to set and obtain specific information on the object
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String name) {
+        this.title = name;
+    }
+
+    public String getPostDate() {
+        return PostDate;
+    }
+
+    public void setPostDate(String PostDate) {
+        this.PostDate = PostDate;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getJLink(){return JLink;}
+
+    public void setJLink(){ this.JLink = JLink; }
+
+    public String getDesc() { return Desc; }
+
+    public void setDesc(String Desc) { this.Desc = Desc; }
+
+}
---
+++ JobPostsAdapter.java
@@ -0,0 +1,55 @@
+package com.example.user.OpusRimor;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class JobPostsAdapter extends RecyclerView.Adapter<JobPostsAdapter.MyViewHolder> {
+
+    private List<JobPost> postingList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, site, PostDate;
+
+        public MyViewHolder(View view) {
+            // connects a variable to an XML text view
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            PostDate = (TextView) view.findViewById(R.id.PostDate);
+            site = (TextView) view.findViewById(R.id.site);
+        }
+    }
+
+
+    public JobPostsAdapter(List<JobPost> postingList) {
+        this.postingList = postingList;
+    }
+        // sends job post object's information to textView
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.job_post_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        JobPost PostList = postingList.get(position);
+        holder.title.setText(PostList.getTitle());
+        holder.site.setText(PostList.getSite());
+        holder.PostDate.setText(PostList.getPostDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return postingList.size();
+    }
+
+
+
+}
