457cd5f7da8acee1003d0426fb0c4d91f0d4dec4
t802 <trevor.lacombe@gmail.com>
2018-04-16 21:38:34 -0400

    2018/04/16 v1
    - added files to new repository

---
+++ app/src/main/java/com/example/user/OpusRimor/Main3Activity.java
@@ -0,0 +1,100 @@
+package com.example.user.OpusRimor;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main3Activity extends AppCompatActivity {
+
+    // collects and displays job postings that match chosen terms
+    // converts string list into a list of jobPost objects
+    // uses a recycler view display objects on XML
+    private List<JobPost> jobList2 = new ArrayList<>();
+    private RecyclerView recyclerView2;
+    private JobPostsAdapter jAdapter2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        recyclerView2 = (RecyclerView) findViewById(R.id.recycler_view2);
+
+        // interface to recycler view
+        jAdapter2 = new JobPostsAdapter(jobList2);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView2.setLayoutManager(mLayoutManager);
+        recyclerView2.setItemAnimator(new DefaultItemAnimator());
+        recyclerView2.setAdapter(jAdapter2);
+
+        compileJobs2();
+
+
+        // row click listener
+        recyclerView2.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView2, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                JobPost PostList2 = jobList2.get(position);
+
+                String JobBody;
+                String titlePick;
+                JobBody = PostList2.getDesc();
+                titlePick = PostList2.getTitle();
+                if(JobBody.length() != 0) {
+                    Intent DescDisplay = new Intent(getBaseContext(), Main4Activity.class);
+                    DescDisplay.putExtra("BodyOfJob", JobBody);
+                    DescDisplay.putExtra("TitlePick", titlePick);
+                    startActivity(DescDisplay);
+                }else{
+                    JobBody = "Sorry bub";
+                        Intent DescDisplay = new Intent(getBaseContext(), Main4Activity.class);
+                        DescDisplay.putExtra("BodyOfJob", JobBody);
+                        startActivity(DescDisplay);
+                }
+            }
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+
+        }));
+
+
+    }
+
+        private void compileJobs2(){
+
+            Intent passJobList = getIntent();
+            ArrayList<String> AllJobs = passJobList.getStringArrayListExtra("ALLJOBS");
+
+
+            String[] AllJPosts = new String[AllJobs.size()];
+
+            for(int i=0; i < AllJobs.size(); i++){
+                AllJPosts[i] = AllJobs.get(i);
+            }
+
+            if(AllJobs.size() == 0){
+                JobPost jobPost = new JobPost("Didn't work", "Site", "now", "BackLink", "BackDesc");
+                jobList2.add(jobPost);
+            }else {
+
+                for (int i = 0; i < AllJobs.size(); i = i + 5) {
+                                            //Job title       Site Source         Post Date
+                    JobPost jobpost = new JobPost(AllJPosts[i], AllJPosts[i + 1], AllJPosts[i + 2],AllJPosts[i + 3], AllJPosts[i + 4]);
+                    jobList2.add(jobpost);
+                }
+            }
+            jAdapter2.notifyDataSetChanged();
+        }
+
+
+}
---
+++ app/src/main/java/com/example/user/OpusRimor/Main4Activity.java
@@ -0,0 +1,35 @@
+package com.example.user.OpusRimor;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+        Intent DescDisplay = getIntent();
+        String JobBody = DescDisplay.getStringExtra("BodyOfJob");
+        String JobTitle = DescDisplay.getStringExtra("TitlePick");
+
+        TextView Description;
+        Description = findViewById(R.id.DescBox);
+        Description.setMovementMethod(new ScrollingMovementMethod());
+        Description.setText(Html.fromHtml(JobBody));
+
+        TextView Title;
+        Title = findViewById(R.id.jobTitle);
+        Title.setText(JobTitle);
+
+    }
+}
